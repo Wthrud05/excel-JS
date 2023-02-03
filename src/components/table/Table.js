bb1e07d650c117754dc4c9c6fdfe773fb721e7ff
Wthrud05 <alex.electrik220@gmail.com>
2023-02-03 20:05:08 +0300

    finist redux and table logic

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,6 +4,9 @@ import {resizeHandler} from './table.resize'
 import {isCell, shouldResize, matrix, nextSelector} from './table.functions'
 import {TableSelection} from './TableSelection'
 import {$} from '../../core/dom'
+import {defaultStyles} from '../../constants'
+import {parse} from '../../core/parse'
+import * as actions from '../../redux/actions'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
@@ -17,7 +20,7 @@ export class Table extends ExcelComponent {
   }
 
   toHTML() {
-    return createTable(200)
+    return createTable(50, this.store.getState())
   }
 
   prepare() {
@@ -29,23 +32,46 @@ export class Table extends ExcelComponent {
 
     this.selectCell(this.$root.find('[data-id="0:0'))
     
-    this.$on('formula:input', text => {
-      this.selection.current.text(text)
+    this.$on('formula:input', value => {
+      this.selection.current
+          .attr('data-value', value)
+          .text(parse(value))
+      this.updateTextInStore(value)
     })
 
     this.$on('formula:done', () => {
       this.selection.current.focus()
+    })
+
+    this.$on('toolbar:applyStyle', value => {
+      this.selection.applyStyle(value)
+      this.$dispatch(actions.applyStyle({
+        value,
+        ids: this.selection.selectedIds
+      }))
     })
   }
 
   selectCell($cell) {
     this.selection.select($cell)
     this.$emit('table:select', $cell)
+
+    const styles = $cell.getStyles(Object.keys(defaultStyles)) 
+    this.$dispatch(actions.changeStyles(styles))
+  }
+
+  async resizeTable(e) {
+    try {
+      const data = await resizeHandler(this.$root, e)
+      this.$dispatch(actions.tableResize(data))
+    } catch (e) {
+      console.error(e)
+    }
   }
 
   onMousedown(e) {
     if (shouldResize(e)) {
-      resizeHandler(this.$root, e)
+      this.resizeTable(e)
     } else if (isCell) {
       const $target = $(e.target)
       if (e.shiftKey) {
@@ -53,7 +79,7 @@ export class Table extends ExcelComponent {
             .map(id => this.$root.find(`[data-id="${id}"]`))
         this.selection.selectGroup($cells)
       } else {
-        this.selection.select($target)
+        this.selectCell($target)
       }
     }
   }
@@ -77,7 +103,13 @@ export class Table extends ExcelComponent {
     }
   }
 
+  updateTextInStore(value) {
+    this.$dispatch(actions.changeText({
+      id: this.selection.current.id(),
+      value
+    }))
+  }
   onInput(event) {
-    this.$emit('table:input', $(event.target))
+    this.updateTextInStore($(event.target).text())
   }
 }
--- src/components/table/table.resize.js
+++ src/components/table/table.resize.js
@@ -1,47 +1,55 @@
 import {$} from '../../core/dom'
 
 export function resizeHandler($root, e) {
-  const $resizer = $(e.target)
-  const $parent = $resizer.closest('[data-type="resizable"]')
-  const type = $resizer.data.resize
-  const id = $parent.data.col
-  const coords = $parent.getCoords()
-  const side = type === 'col' ? 'bottom' : 'right'
-  let value
+  return new Promise(resolve => {
+    const $resizer = $(e.target)
+    const $parent = $resizer.closest('[data-type="resizable"]')
+    const type = $resizer.data.resize
+    const id = $parent.data.col
+    const coords = $parent.getCoords()
+    const side = type === 'col' ? 'bottom' : 'right'
+    let value
 
-  $resizer.css({
-    opacity: '1',
-    [side]: '-5000px'
-  })
-  
-  document.onmousemove = e => {
-    if (type === 'col') {
-      const delta = e.pageX - coords.right
-      value = coords.width + delta
-      $resizer.css({right: -delta + 'px'})
-    } else {
-      const delta = e.pageY - coords.bottom
-      value = coords.height + delta
-      $resizer.css({bottom: -delta + 'px'})
-    }
-  }
-
-  document.onmouseup = () => {
-    document.onmousemove = null
-    document.onmouseup = null
-    
-    if (type === 'col') {
-      $parent.css({width: value + 'px'})
-      $root.findAll(`[data-${type}="${id}"]`)
-          .forEach(el => el.style.width = value + 'px')
-    } else {
-      $parent.css({height: value + 'px'})
-    }
-    
     $resizer.css({
-      opacity: 0,
-      bottom: 0,
-      right: 0,
+      opacity: '1',
+      [side]: '-5000px'
     })
-  }
+    
+    document.onmousemove = e => {
+      if (type === 'col') {
+        const delta = e.pageX - coords.right
+        value = coords.width + delta
+        $resizer.css({right: -delta + 'px'})
+      } else {
+        const delta = e.pageY - coords.bottom
+        value = coords.height + delta
+        $resizer.css({bottom: -delta + 'px'})
+      }
+    }
+
+    document.onmouseup = () => {
+      document.onmousemove = null
+      document.onmouseup = null
+      
+      if (type === 'col') {
+        $parent.css({width: value + 'px'})
+        $root.findAll(`[data-${type}="${id}"]`)
+            .forEach(el => el.style.width = value + 'px')
+      } else {
+        $parent.css({height: value + 'px'})
+      }
+
+      resolve({
+        value,
+        type,
+        id: $parent.data[type]
+      })
+      
+      $resizer.css({
+        opacity: 0,
+        bottom: 0,
+        right: 0,
+      })
+    }
+  })
 }
